8809abcfdcd2e735dc439635b459ac5e704f8f26
Torre Matthew <torre.matthew@gmail.com>
2018-12-22 21:22:56 -0500

    node server file and folders

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+const axios = require("axios");
+
+// Require all models
+// const db = require("./models");
+
+const PORT = 3000;
+
+// Initialize Express
+const app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+// mongoose.connect("", { "": true });
+
+// Routes
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
