bd4ce29dbc968ddc1f1f13166fbe1433dbce2382
DChang <chang19d@gmail.com>
2018-12-23 19:13:50 -0500

    added utils folder, and API.js file which has two methods of searching the edamam API, by ingredients or by recipe ID

---
+++ app/src/utils/API.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const BASEURL = "https://api.edamam.com/search?q=";
+const APIKEY = "&app_key=e110d557a91aacde45b67d166fff1374";
+const appID = "&app_id=82a2955b";
+const BASEURI = "https://api.edamam.com/search?r="
+const URI = "http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_"
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + query + appID + APIKEY);
+  },
+  seachByID: function(id){
+    return axios.get(BASEURI + URI + id + appID + APIKEY);
+  }
+};
