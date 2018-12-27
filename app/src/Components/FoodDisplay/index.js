1bf7e6ed2f90af057a55f942b7d4042a6ea5782a
Brian <40767783+brishaw@users.noreply.github.com>
2018-12-26 21:47:32 -0500

    updated rest of pg2 and added info graphic placeholders to images on slider...

--- app/src/Components/FoodDisplay/index.js
+++ app/src/Components/FoodDisplay/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import "./style.css";
 import resultImage1 from "./result1.jpg";
 import resultImage2 from "./result2.jpg";
+import resultImage3 from "./result3.jpg";
+import resultImage4 from "./result4.jpg";
 import result_icon1 from "./result_icon1.png";
 import result_icon2 from "./result_icon2.png";
+import result_icon3 from "./result_icon3.png";
+import result_icon4 from "./result_icon4.png";
 function FoodDisplay() {
   return (
     <div className="pp-fd">
       <div className="container">
         <div className="row">
           <div className="col s12 center">
-            <h2>Here are some meals to consider</h2>
+            <h1>Here are some meals to consider</h1>
           </div>
         </div>
         <div className="row">
@@ -23,7 +27,7 @@ function FoodDisplay() {
                 <img src={result_icon1} />
               </div>
               <div className="pp-fd-link">
-                <a href="#">See Product</a>
+                <a href="#">See Recipe</a>
               </div>
             </div>
           </div>
@@ -34,6 +38,34 @@ function FoodDisplay() {
               </div>
               <div className="pp-fd-icon">
                 <img src={result_icon2} />
+              </div>
+              <div className="pp-fd-link">
+                <a href="#">See Product</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s6">
+            <div className="pp-fd-results">
+              <div>
+                <img src={resultImage3} />
+              </div>
+              <div className="pp-fd-icon">
+                <img src={result_icon3} />
+              </div>
+              <div className="pp-fd-link">
+                <a href="#">See Recipe</a>
+              </div>
+            </div>
+          </div>
+          <div className="col s6">
+            <div className="pp-fd-results">
+              <div>
+                <img src={resultImage4} />
+              </div>
+              <div className="pp-fd-icon">
+                <img src={result_icon4} />
               </div>
               <div className="pp-fd-link">
                 <a href="#">See Product</a>
