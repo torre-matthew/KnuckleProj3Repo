614a8d6cf4acc71aee9aa56e13c038493d712d70
DChang <chang19d@gmail.com>
2018-12-28 12:55:32 -0500

    added create function for adding new recipe to DB, added the post route to our API, and added an axios post method to our API for our front end.

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -1,6 +1,15 @@
 const db = require("../db/models");
 
+//This function takes in a request body with name, image, and recipeID, and then
+//inserts a new recipe into the savedRecipes collection
+let saveRecipe = (req, res) => {
+    db.SavedRecipes.create( { name: req.body.name, image: req.body.image, recipeID: req.body.recipeID} )
+    .then(data => res.json(data))
+    .catch(err => console.log(err));
+}
+
+
 // Defining methods for the recipesController
 module.exports = {
-  
+    saveRecipe:saveRecipe
 };
