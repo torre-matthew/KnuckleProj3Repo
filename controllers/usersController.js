e819182797975915bf743f7bb965d332ac463817
Torre Matthew <torre.matthew@gmail.com>
2018-12-28 20:24:06 -0500

    add saved recipe to users savedRecipe array

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,8 +9,29 @@ let findUsersSavedRecipes = (req, res) => {
 }
 
 
+let associateSavedRecipesToUser = (req, res) => {
+// Look for the saved recipe that has the recipeID that we store from the Edemam API of the recipe the user is saving.
+    db.SavedRecipes.find({"recipeID": req.body.recipeID})
+    .then(data => {
+// Then lookup the user based on the stored googleID and push the mongodb id of that recipe into the savedRecipes array of that user.
+        db.Users.findOneAndUpdate({"googleId": req.body.googleID}, {$push: {savedRecipes: data[0]._id}})
+        .then(updatedUserRecord => {
+            res.json(updatedUserRecord);
+        })
+        .catch(err => {
+            console.error(err);   
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+}
+
+
 // Defining methods for the usersController
 module.exports = {
     usersSavedRecipes: findUsersSavedRecipes,
+    saveARecipe: associateSavedRecipesToUser
   
 };
