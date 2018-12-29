f34788fa66f9c5e8b2cd2dbdd06719ae01541b44
Quy Nguyen <nguyenqb24@gmail.com>
2018-12-28 22:54:35 -0500

    postData added

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -1,13 +1,38 @@
 import React, {Component} from 'react';
 
 import GoogleLogin from 'react-google-login';
-
+import {PostData} from '../services/PostData';
 
    
 class Welcome extends Component{
+    signup(res, type){
+        let postData;
+        
+    if (type === 'google' && res.w3.U3) {
+        postData = {
+          name: res.w3.ig,
+          provider: type,
+          email: res.w3.U3,
+          provider_id: res.El,
+          token: res.Zi.access_token,
+          provider_pic: res.w3.Paa
+        }
+    }
+
+    if (postData) {
+        PostData('signup', postData).then((result) => {
+          let responseJson = result;
+          sessionStorage.setItem("userData", JSON.stringify(responseJson));
+          this.setState({redirect: true});
+        });
+      } 
+    
+
+}
     render(){
         const responseGoogle = (response) => {
             console.log(response);
+            this.signup(response, "google");
           }
            
         return(
