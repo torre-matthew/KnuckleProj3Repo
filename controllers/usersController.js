c49398260289a6bdcb2fda63a20fe34095e0ca1e
Torre Matthew <torre.matthew@gmail.com>
2018-12-28 19:41:53 -0500

    stashing

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,8 +9,30 @@ let findUsersSavedRecipes = (req, res) => {
 }
 
 
+let associateSavedRecipesToUser = (req, res) => {
+    //Associate Associaate the meatloaf dish to Quy's user.
+    db.SavedRecipes.find({"recipeID": req.body.recipeID})
+    .then(data => {
+    //Update Quy's record to include a new saved recipe
+        // res.json(data);
+        db.Users.findOneAndUpdate({"googleId": req.body.googleID}, {$push: {savedRecipes: data[0]._id}})
+        .then(updatedUserRecord => {
+            res.json(updatedUserRecord);
+        })
+        .catch(err => {
+            console.error(err);   
+        });
+    })
+    .catch(err => {
+        console.error(err);
+    });
+    
+}
+
+
 // Defining methods for the usersController
 module.exports = {
     usersSavedRecipes: findUsersSavedRecipes,
+    saveARecipe: associateSavedRecipesToUser
   
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -5,7 +5,10 @@ const usersController = require("../../controllers/usersController");
 router.route("/saved-recipes")
 // Get all savedRecipes based on the googleID of user. GoogleId should be unique so this should be safe.
 // endpoint looks like: /api/user/saved-recipes
-  .get(usersController.usersSavedRecipes); 
+  .get(usersController.usersSavedRecipes);
+  
+router.route("/saverecipe")
+    .get(usersController.saveARecipe);
 
 
 // Matches with "/api/savedRecipes/:id"
