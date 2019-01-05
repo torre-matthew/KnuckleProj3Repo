2bcb140bf3b0196e555afe3cd9ec5dd845cafbf4
Torre Matthew <torre.matthew@gmail.com>
2019-01-05 11:24:07 -0500

    stashing

--- app/src/components/Welcome.js
+++ app/src/components/Welcome.js
@@ -3,40 +3,14 @@ import GoogleLogin from 'react-google-login';
 // import axios from "axios";
 import PPAPI from "../utils/pocketPantryAPI";
 
-import {PostData} from '../services/PostData';
 
    
 class Welcome extends Component{
-    signup(res, type){
-        let postData;
-        
-    if (type === 'google' && res.w3.U3) {
-        postData = {
-          name: res.w3.ig,
-          provider: type,
-          email: res.w3.U3,
-          provider_id: res.El,
-          token: res.Zi.access_token,
-          provider_pic: res.w3.Paa
-        }
-    }
-
-    if (postData) {
-        PostData('signup', postData).then((result) => {
-          let responseJson = result;
-          sessionStorage.setItem("userData", JSON.stringify(responseJson));
-          this.setState({redirect: true});
-        });
-      } 
-    
-
-}
     render(){
         const responseGoogle = (response) => {
             console.log(response);
             PPAPI.createUser(response.profileObj.name, response.profileObj.googleId, response.profileObj.imageUrl, response.profileObj.email)
             PPAPI.getUsersSavedRecipes(response.profileObj.googleId); 
-            this.signup(response, "google");
           }
            
         return(
